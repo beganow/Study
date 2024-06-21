9a984a3f9213e829ef5c52773c125bbfaa06def5
Pavel Obodov <95656497+beganow@users.noreply.github.com>
2024-06-21 19:27:20 +0300

    Delete CourseWorks directory

--- CourseWorks/Cource_2_JS+React/react-yourstep/src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import CreateStore from "./CreateProducts";
-import useReducer from "./userSlice";
-
-const store = configureStore({
-  reducer: { product: CreateStore, user: useReducer },
-});
-
-export default store;
